d4f4d4d2d2f5e47f6d79f614e62840c656ef4e1b
Ran <ranmalach82@gmail.com>
2021-11-01 10:55:37 +0200

    Adding Implementation code

--- stats.c
+++ stats.c
@@ -39,42 +39,118 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
+  unsigned int N = SIZE;
+  print_array(test, N);
+  print_statistics(test, N);
+  
 }
 
 /* Statistics and Printing Functions Go Here */
 
 void print_array(const unsigned char * A
                   , const unsigned int N){
+  printf("~~~~~~~~\n   Array\n~~~~~~~~\n[ ");
+  for(int i = 0 ; i < N ; ++i)
+    printf("%d ", A[i]);
+  printf("]\n");
 }
 
 void print_statistics( const unsigned char *A
                         , const unsigned int N){
+  unsigned char max = find_maximum(A, N);
+  unsigned char min = find_minimum(A, N);
+  unsigned char mean = find_mean(A, N);
+  unsigned char median = find_median(A, N);
+
+  printf("~~~~~~~~~~~~~~~~~~\n");
+  printf(" Array statistics\n");
+  printf("~~~~~~~~~~~~~~~~~~\n");
+  printf("Max Element  = %d\n", max);
+  printf("Min Element  = %d\n", min);
+  printf("Mean Value   = %d\n", mean);
+  printf("Median Value = %d\n", median);
+  printf("~~~~~~~~~~~~~~~~~~\n");
+
 }
 
 const unsigned char find_maximum(const unsigned char* A
                                             , const unsigned int N ){
+  unsigned char max_val = 0;
+  for(int i = 0 ; i < SIZE ; ++i){
+    if(A[i] > max_val){
+      max_val = A[i];
+    }
+  }
+  return max_val;
 }
 
 const unsigned char find_minimum(const unsigned char* A
                                             , const unsigned int N ){
+  unsigned char min_val = 255;
+  for(int i = 0 ; i < SIZE ; ++i){
+    if(A[i] < min_val){
+      min_val = A[i];
+    }
+  }
+  return min_val;
 }
 
 const unsigned char find_mean(const unsigned char* A
                                       , const unsigned int N ){
+  unsigned int sum = 0;
+  for(int i = 0 ; i < SIZE ; ++i){
+    sum += (unsigned int)A[i];
+  }
+  return (unsigned char)(sum / SIZE);
 }
 
 
 const unsigned char find_median(const unsigned char* A
                                         , const unsigned int N ){
+    /* copy and sort the array */
+
+  unsigned char sorted_A[SIZE];
+  for(int i = 0 ; i < SIZE ; ++i){
+    sorted_A[i] = A[i];
+  }
+
+  sort_array(sorted_A, SIZE);
+
+  if(SIZE & 1) // Case odd array size
+    return sorted_A[SIZE /2];
+  return (sorted_A[(SIZE - 1) /2] + sorted_A[SIZE /2]) /2;
 }
 
 /* Add other Implementation File Code Here */
 
 
 void swap(unsigned char* a, unsigned char* b){
+/*  swap func for quicksort */
+  int temp = *a;
+  *a = *b;
+  *b = temp;
 }
 
 void sort_array(unsigned char* A, unsigned int N){
+/*  O(nlogn) Quick Sort Algorithm implementation  */
+    if(N < 2)
+        return;
+    int l = 0;
+    int r = N - 2;
+    int p = N - 1;
+    while(r > l){
+        if(A[l] >= A[p]) 
+          ++l;
+        if(A[r] <= A[p]) 
+          --r;
+        if(A[r] >= A[p] && A[l] <= A[p] && r > l)
+            swap(&A[l], &A[r]);
+    }
+    if(A[r] > A[p]) 
+      ++r;
+    swap(&A[r], &A[p]);
+    sort_array(A, r);
+    sort_array(&A[r + 1], N - (r + 1));
 }
 
 
